ee202046a07192242c16d4b0be6c71cca7c356af
Nikolay Platonov <juninhonvp@gmail.com>
2021-08-22 17:11:50 +0300

    Update getactivate.js

--- getactivate.js
+++ getactivate.js
@@ -1,12 +1,13 @@
-var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;if (MutationObserver) 
-var observer = new MutationObserver(function(mutations) {
-    setTimeout(function () {
-        var bonus = document.getElementsByClassName('claimable-bonus__icon');
-        for (var i = 0;i<bonus.length;i++) {
-            if (i == 0) console.log('Claimed!');
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+if(MutationObserver) 
+    var observer = new MutationObserver(function(mutations) {
+        setTimeout(function () {
+            var bonus = document.getElementsByClassName('claimable-bonus__icon');
+            for(var i = 0; i < bonus.length; i++) {
             	bonus[i].click();
+                console.log('Claimed!');
             }
-    }, Math.random() * 1000 + 2500);
-});
+        }, Math.random() * 1000 + 2500);
+    });
 observer.observe(document.body, {childList: true, subtree: true});
 console.log('Auto claimer is enabled.');
